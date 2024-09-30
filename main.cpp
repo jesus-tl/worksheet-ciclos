94fe9dac7ffb1806660d94b883577fc054b0b36e
jesus-tl <jesus.torres21@upr.edu>
2024-09-30 17:51:18 +0000

    primera secuencia

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 //Jesus Torres , jesus-tl, 801203215
 
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
 
 int main(){
 
 
+//Primera Secuencia 10 a -10
+
+int secuencia_1;
+
+cout << "- Primera Secuencia: ";
+
+for (secuencia_1 = 11 ; secuencia_1 > -11; secuencia_1--){
+
+  if (secuencia_1 % 2 == 0){ //Verificar si numero es par
+
+  cout << secuencia_1 << ", ";
+
+  }
+}
+
+cout << endl;
 
 
-
-    return 0;
+return 0;
 }
